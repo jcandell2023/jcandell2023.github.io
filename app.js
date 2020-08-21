8b3ef55f9f6611b46c0eaca16d5248db03ef8e5a
Jeff Candell <jcandell@wustl.edu>
2020-08-21 13:21:39 -0400

    Styled a bit

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let elems = document.querySelectorAll('.sidenav')
+  const options = {}
+  let instances = M.Sidenav.init(elems, options)
+})
