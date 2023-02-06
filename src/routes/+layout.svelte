f88dfa1e37b84365f350bd57dfdabad37edf865b
Jeff Candell <jcandell@wustl.edu>
2023-02-05 22:03:25 -0600

    some content

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-primary">
+	<div class="container">
+        <span class="navbar-brand h1 m-0">Jeff Candell</span>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbar"
+			aria-controls="navbar"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon" />
+		</button>
+		<div class="collapse navbar-collapse" id="navbar">
+			<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="/">Home</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<div class="container">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import img from '$lib/images/profile.jpg';
+</script>
+
+<div class="row mt-3">
+	<div class="col">
+		<img id="profile_img" src={img} alt="" />
+	</div>
+	<div class="col">
+		<div class="bg-light border rounded-3 p-5">
+			<h1 class="text-center">I'm Jeff Candell</h1>
+			<p>As an avid runner and coder, I'm always looking for new ways to explore.</p>
+		</div>
+	</div>
+</div>
+
+<style>
+	#profile_img {
+		width: 100%;
+	}
+</style>
