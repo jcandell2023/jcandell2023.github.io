d9a0321af71751c872301777e12d29dc4481f90b
jcandell2023 <59771419+jcandell2023@users.noreply.github.com>
2025-06-29 22:04:25 -0400

    Update CSS (#10)
    Co-authored-by: Jeff Candell <jeff@candell.org>

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -1,9 +1,11 @@
+<script lang="ts">
+	import { siteData } from '../data';
+</script>
+
 <h2 id="about-me">About Me</h2>
 <div class="grid">
-	<div class="s12 m6 l4">
-		<img src="./images/budapest.avif" alt="Me in Budapest" class="responsive" />
+	<div class="s12 m6">
+		<p class="large-text">{siteData.aboutMe}</p>
 	</div>
-	<div class="s12 m6 l4">
-		<img src="./images/running.avif" alt="Me running" class="responsive" />
-	</div>
+	<div class="s12 m6"></div>
 </div>
